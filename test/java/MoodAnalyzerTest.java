29ff3a511a8c8ae187ab852e355e97dc5180fd66
saitarun135 <saitarun135@gm.com>
2021-03-22 19:52:01 +0530

    Enum-custom exceptions

--- test/java/MoodAnalyzerTest.java
+++ test/java/MoodAnalyzerTest.java
@@ -1,26 +1,51 @@
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 public class MoodAnalyzerTest {
     @Test
-    public void givenMessage_Contain_WhenSad_ShouldReturn_Sad() {
-        MoodAnalyzer moodAnalyzer = new MoodAnalyzer("This is a Sad Message");
-        String mood = moodAnalyzer.analyseMood();
-        Assert.assertEquals("SAD",mood);
-
+    public void givenMessage_WhenSad_ShouldReturn_Sad() {
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer("I am in Sad Mood");
+        String mood;
+        try {
+            mood = moodAnalyzer.analyseMood();
+            Assert.assertEquals("SAD", mood);
+        } catch (MoodAnalyzerException e) {
+        }
     }
-
     @Test
-    public void givenMessage_Contain_WhenNotSad_ShouldReturn_Happy() {
-        MoodAnalyzer moodAnalyzer = new MoodAnalyzer("This is Happy Messgae");
-        String mood = moodAnalyzer.analyseMood();
-        Assert.assertEquals("HAPPY",mood);
+    public void givenMessage_WhenNotSad_ShouldReturn_Happy() {
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer("I am in any Mood");
+        String mood;
+        try {
+            mood = moodAnalyzer.analyseMood();
+            Assert.assertEquals("HAPPY", mood);
+        } catch (MoodAnalyzerException e) {
+        }
+
     }
+
     @Test
     public void givenNullMood_ShouldReturn_Happy() {
         MoodAnalyzer moodAnalyzer = new MoodAnalyzer(null);
-        String mood = moodAnalyzer.analyseMood();
-        Assert.assertEquals("HAPPY",mood);
+        String mood;
+        try {
+            ExpectedException exceptionRule = ExpectedException.none();
+            exceptionRule.expect(MoodAnalyzerException.class);
+            mood = moodAnalyzer.analyseMood();
+            Assert.assertEquals("HAPPY", mood);
+        } catch (MoodAnalyzerException e) {
+            e.printStackTrace();
+        }
     }
 
+    @Test
+    public void givenNullMood_ShouldThrow_Exception() {
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer(null);
+        try {
+            moodAnalyzer.analyseMood(null);
+        } catch (MoodAnalyzerException e) {
+            Assert.assertEquals(MoodAnalyzerException.exceptionType.ENTERED_NULL, e.type);
+        }
+    }
 }
