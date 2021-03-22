94d9482104d7d2a942d922f4fc997c26196e6515
saitarun135 <saitarun135@gm.com>
2021-03-22 19:53:50 +0530

    Refactor the code

--- main/java/MoodAnalyzer.java
+++ main/java/MoodAnalyzer.java
@@ -1,18 +1,25 @@
 public class MoodAnalyzer {
     private String message;
+
     public MoodAnalyzer(String message) {
         this.message = message;
     }
 
-    public String analyseMood() {
+    public String analyseMood(String message) throws MoodAnalyzerException {
+        this.message = message;
+        return analyseMood();
+    }
+
+    public String analyseMood() throws MoodAnalyzerException {
         try {
+            if (message.length() == 0)
+                throw new MoodAnalyzerException(MoodAnalyzerException.exceptionType.ENTERED_EMPTY, "Please enter Proper Mood");
             if (message.contains("Sad"))
                 return "SAD";
             else
                 return "HAPPY";
-        }
-        catch(NullPointerException ne){
-            return "HAPPY";
+        } catch (NullPointerException e) {
+            throw new MoodAnalyzerException(MoodAnalyzerException.exceptionType.ENTERED_NULL, "Please enter Proper Mood");
         }
     }
 }
