abb1de31ab61bfac1abfcc1de7bf0c67aaa26060
saitarun135 <saitarun135@gm.com>
2021-03-22 12:16:29 +0530

    Happy or sad greetings

--- main/java/MoodAnalyzer.java
+++ main/java/MoodAnalyzer.java
@@ -1,2 +1,13 @@
-#WELCOME TO THE MOOD ANALYZER
+public class MoodAnalyzer {
+    private String message;
+    public MoodAnalyzer(String message) {
+        this.message = message;
+    }
 
+    public String analyseMood() {
+        if(message.contains("Sad"))
+            return "SAD";
+        else
+            return "HAPPY";
+    }
+}
