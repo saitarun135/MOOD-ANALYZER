be92644ff40038beaa181360fc838eecb54baead
saitarun135 <saitarun135@gm.com>
2021-03-22 12:12:55 +0530

    Greetings

--- main/java/MoodAnalyzer.java
+++ main/java/MoodAnalyzer.java
@@ -1,14 +1,2 @@
- public class MoodAnalyzer {
-        private String message;
-        public MoodAnalyzer(String message) {
-            this.message = message;
-        }
-
-        public String analyseMood() {
-            if(message.contains("Sad"))
-                return "SAD";
-            else
-                return "HAPPY";
-        }
-    }
+#WELCOME TO THE MOOD ANALYZER
 
