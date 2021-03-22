939137693b54044820377440c7131647e72f48b7
saitarun135 <saitarun135@gm.com>
2021-03-22 12:09:48 +0530

    adding sad and happy messages

---
+++ test/java/MoodAnalyzerTest.java
@@ -0,0 +1,20 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MoodAnalyzerTest {
+    @Test
+    public void givenMessage_Contain_WhenSad_ShouldReturn_Sad() {
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer("This is a Sad Message");
+        String mood = moodAnalyzer.analyseMood();
+        Assert.assertEquals("SAD",mood);
+
+    }
+
+    @Test
+    public void givenMessage_Contain_WhenNotSad_ShouldReturn_Happy() {
+        MoodAnalyzer moodAnalyzer = new MoodAnalyzer("This is Happy Messgae");
+        String mood = moodAnalyzer.analyseMood();
+        Assert.assertEquals("HAPPY",mood);
+    }
+
+}
